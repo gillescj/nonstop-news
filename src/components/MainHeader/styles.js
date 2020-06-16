e5654573a160486734cf480077f35215c57440ea
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-16 02:45:28 -0400

    Modify padding, colour, background, box-shadow

--- src/components/MainHeader/styles.js
+++ src/components/MainHeader/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
+import { COLOURS } from '../../styling';
 
 export const Container = styled.div`
     display: flex;
-    background: red;
+    padding: 1rem;
+    color: white;
+    background: ${COLOURS.darkPurple};
+    box-shadow: 0 1px 2px 0 hsla(206, 6%, 25%, 0.3), 0 2px 6px 2px hsla(206, 6%, 25%, 0.3);
 `;

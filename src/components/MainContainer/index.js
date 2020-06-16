ddaee0eee457556245d12f2b47fbcb65bc7e87b5
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-16 04:51:16 -0400

    Add SecondaryNewsCard component with dummy props

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from './styles';
 import PrimaryNewsCard from '../NewsCards/PrimaryNewsCard';
+import SecondaryNewsCard from '../NewsCards/SecondaryNewsCard';
 
 const MainContainer = () => {
     return (
@@ -11,6 +12,15 @@ const MainContainer = () => {
                 title="Forex Today: Double stimulus talk downs dollar ahead of Powell's power-play, US retail sales"
                 description={`Here is what you need to know on Tuesday, June 16: The market mood is \"risk-on\" once again, as the Federal Reserve announced buying corporate bonds an`}
                 publishedAt="2020-06-16T06:07:43Z"
+                url="https://www.fxstreet.com/news/forex-today-double-stimulus-talk-downs-dollar-ahead-of-powells-power-play-us-retail-sales-202006160607"
+            />
+            <SecondaryNewsCard
+                sourceName="FXStreet"
+                author="Yohay Elam"
+                title="Forex Today: Double stimulus talk downs dollar ahead of Powell's power-play, US retail sales"
+                description={`Here is what you need to know on Tuesday, June 16: The market mood is \"risk-on\" once again, as the Federal Reserve announced buying corporate bonds an`}
+                publishedAt="2020-06-16T06:07:43Z"
+                url="https://www.fxstreet.com/news/forex-today-double-stimulus-talk-downs-dollar-ahead-of-powells-power-play-us-retail-sales-202006160607"
             />
         </Container>
     );

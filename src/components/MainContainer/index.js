ea30a44a5bdc3e7acd87745eb353dd48484d1bfa
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-16 17:17:56 -0400

    Add news cards to render

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, SecondaryNewsCardContainer } from './styles';
 import PrimaryNewsCard from '../NewsCards/PrimaryNewsCard';
 import SecondaryNewsCard from '../NewsCards/SecondaryNewsCard';
 
@@ -14,7 +14,7 @@ const MainContainer = () => {
                 publishedAt="2020-06-16T06:07:43Z"
                 url="https://www.fxstreet.com/news/forex-today-double-stimulus-talk-downs-dollar-ahead-of-powells-power-play-us-retail-sales-202006160607"
             />
-            <SecondaryNewsCard
+            <PrimaryNewsCard
                 sourceName="FXStreet"
                 author="Yohay Elam"
                 title="Forex Today: Double stimulus talk downs dollar ahead of Powell's power-play, US retail sales"
@@ -22,6 +22,24 @@ const MainContainer = () => {
                 publishedAt="2020-06-16T06:07:43Z"
                 url="https://www.fxstreet.com/news/forex-today-double-stimulus-talk-downs-dollar-ahead-of-powells-power-play-us-retail-sales-202006160607"
             />
+            <SecondaryNewsCardContainer>
+                <SecondaryNewsCard
+                    sourceName="FXStreet"
+                    author="Yohay Elam"
+                    title="Forex Today: Double stimulus talk downs dollar ahead of Powell's power-play, US retail sales"
+                    description={`Here is what you need to know on Tuesday, June 16: The market mood is \"risk-on\" once again, as the Federal Reserve announced buying corporate bonds an`}
+                    publishedAt="2020-06-16T06:07:43Z"
+                    url="https://www.fxstreet.com/news/forex-today-double-stimulus-talk-downs-dollar-ahead-of-powells-power-play-us-retail-sales-202006160607"
+                />
+                <SecondaryNewsCard
+                    sourceName="FXStreet"
+                    author="Yohay Elam"
+                    title="Forex Today: Double stimulus talk downs dollar ahead of Powell's power-play, US retail sales"
+                    description={`Here is what you need to know on Tuesday, June 16: The market mood is \"risk-on\" once again, as the Federal Reserve announced buying corporate bonds an`}
+                    publishedAt="2020-06-16T06:07:43Z"
+                    url="https://www.fxstreet.com/news/forex-today-double-stimulus-talk-downs-dollar-ahead-of-powells-power-play-us-retail-sales-202006160607"
+                />
+            </SecondaryNewsCardContainer>
         </Container>
     );
 };

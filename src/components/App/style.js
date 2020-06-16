6e91aa11548b3ad918e4db46f8545a52841f2fff
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-16 04:05:29 -0400

    Add align-items

--- src/components/App/style.js
+++ src/components/App/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: grid;
+    align-items: start;
     min-height: 100vh;
     grid-template-rows: auto 1fr auto;
 `;

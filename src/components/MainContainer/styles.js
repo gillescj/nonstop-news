b5813c00b9685aae72e22495453cdbf45ad602a3
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-16 17:17:21 -0400

    Modify layout of news cards

--- src/components/MainContainer/styles.js
+++ src/components/MainContainer/styles.js
@@ -1,8 +1,17 @@
 import styled from 'styled-components';
 import { QUERIES } from '../../styling';
+import SecondaryNewsCard from '../NewsCards/SecondaryNewsCard';
 
 export const Container = styled.div`
     display: grid;
     justify-self: center;
-    grid-template-columns: minmax(auto, ${QUERIES.maxWidth});
+    grid-gap: 1rem;
+    padding: 1rem;
+    max-width: ${QUERIES.maxWidth};
+    grid-template-columns: minmax(15rem, 35rem) minmax(5rem, 18rem) minmax(5rem, 18rem);
+`;
+
+export const SecondaryNewsCardContainer = styled.div`
+    display: grid;
+    grid-gap: 1rem;
 `;

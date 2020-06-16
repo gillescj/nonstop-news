f2c7b8e3c35f30b9f74b160c4488d6219941a4b2
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-16 04:06:08 -0400

    Add PrimaryNewsCard in render, with dummy values

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Container } from './styles';
+import PrimaryNewsCard from '../NewsCards/PrimaryNewsCard';
 
 const MainContainer = () => {
-    return <Container>MainContainer</Container>;
+    return (
+        <Container>
+            <PrimaryNewsCard
+                sourceName="FXStreet"
+                author="Yohay Elam"
+                title="Forex Today: Double stimulus talk downs dollar ahead of Powell's power-play, US retail sales"
+                description={`Here is what you need to know on Tuesday, June 16: The market mood is \"risk-on\" once again, as the Federal Reserve announced buying corporate bonds an`}
+                publishedAt="2020-06-16T06:07:43Z"
+            />
+        </Container>
+    );
 };
 
 export default MainContainer;

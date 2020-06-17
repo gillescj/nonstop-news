b772a5c97428e36fa135d2241d15527be738d523
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-17 18:40:08 -0400

    Add media queries - medium and small displays

--- src/components/MiddleContent/styles.js
+++ src/components/MiddleContent/styles.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
+import { QUERIES } from 'styling';
 
 export const Container = styled.div`
     display: grid;
+    justify-content: center;
     grid-gap: 1rem;
     padding: 1rem;
-    grid-template-columns: repeat(4, minmax(15rem, 20rem));
+    grid-template-columns: repeat(4, minmax(15rem, 25rem));
+    @media (${QUERIES.medium}) {
+        grid-template-columns: repeat(2, minmax(15rem, 25rem));
+    }
+    @media (${QUERIES.small}) {
+        grid-template-columns: repeat(1, minmax(15rem, 30rem));
+    }
 `;
--- src/components/TopContent/styles.js
+++ src/components/TopContent/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { QUERIES } from 'styling';
 
 export const Container = styled.div`
     display: grid;
@@ -6,9 +7,26 @@ export const Container = styled.div`
     grid-gap: 1rem;
     padding: 1rem;
     grid-template-columns: minmax(15rem, 35rem) minmax(15rem, 20rem) minmax(15rem, 20rem);
+    @media (${QUERIES.medium}) {
+        grid-template-columns: minmax(15rem, 30rem) minmax(15rem, 20rem);
+    }
+    @media (${QUERIES.small}) {
+        grid-template-columns: minmax(15rem, 30rem);
+    }
 `;
 
 export const SecondaryNewsCardContainer = styled.div`
     display: grid;
     grid-gap: 1rem;
+    justify-content: center;
+    justify-items: center;
+    /* justify-self: center; */
+    @media (${QUERIES.medium}) {
+        grid-template-columns: repeat(2, minmax(15rem, 30rem));
+        grid-column: 1 / span 2;
+    }
+    @media (${QUERIES.small}) {
+        grid-template-columns: minmax(15rem, 30rem);
+        grid-column: 1;
+    }
 `;

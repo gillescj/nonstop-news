a4309351c76340a07770c1246b17bd75683aaf61
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-16 22:33:59 -0400

    Add banners to header

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Container, HeaderContent } from './styles';
+import { Container, OuterBanner, MiddleBanner, HeaderContent } from './styles';
 import Navigation from '../Navigation';
 
 const MainHeader = () => {
     return (
         <Container>
-            <HeaderContent>
-                <Navigation />
-            </HeaderContent>
+            <OuterBanner />
+            <MiddleBanner>
+                <HeaderContent>
+                    <Navigation />
+                </HeaderContent>
+            </MiddleBanner>
+            <OuterBanner />
         </Container>
     );
 };
--- src/components/MainHeader/styles.js
+++ src/components/MainHeader/styles.js
@@ -2,11 +2,19 @@ import styled from 'styled-components';
 import { QUERIES, COLOURS } from 'styling';
 
 export const Container = styled.header`
+    box-shadow: 0 1px 2px 0 hsla(206, 6%, 25%, 0.3), 0 2px 6px 2px hsla(206, 6%, 25%, 0.3);
+`;
+
+export const OuterBanner = styled.div`
+    padding: 0.5rem;
+    background: ${COLOURS.lightPurple};
+`;
+
+export const MiddleBanner = styled.div`
     display: grid;
     justify-content: center;
     grid-template-columns: minmax(1rem, ${QUERIES.maxWidth});
     background: ${COLOURS.darkPurple};
-    box-shadow: 0 1px 2px 0 hsla(206, 6%, 25%, 0.3), 0 2px 6px 2px hsla(206, 6%, 25%, 0.3);
 `;
 
 export const HeaderContent = styled.div`

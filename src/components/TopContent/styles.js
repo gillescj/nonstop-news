0c154107fcce5a88315593e0bdbc7f400e9b240b
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-16 20:04:43 -0400

    Modify spacing - center

--- src/components/TopContent/styles.js
+++ src/components/TopContent/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: grid;
+    justify-content: center;
     grid-gap: 1rem;
     padding: 1rem;
     grid-template-columns: minmax(15rem, 35rem) minmax(15rem, 20rem) minmax(15rem, 20rem);

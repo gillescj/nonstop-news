8996178849866e5209b25f91201d57b03129d7a6
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-23 18:28:43 -0400

    Add dropdowns for smaller screen media queries

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {
     Container,
     Logo,
+    SectionsContainer,
     CategoryList,
     CategoryDropdown,
     CountryDropdown,
+    SectionDropdown,
 } from './styles';
 import acceptedCountries from 'utils/acceptedCountries';
 
@@ -20,24 +22,49 @@ const Navigation = () => {
     return (
         <Container>
             <Logo>Nonstop News</Logo>
-            <CategoryList>
-                <li>Business</li>
-                <li>Entertainment</li>
-                <li>Health</li>
-                <li>Science</li>
-                <li>Sports</li>
-                <li>Technology</li>
-            </CategoryList>
-            <CategoryDropdown>
-                <option>General</option>
-                <option>Business</option>
-                <option>Entertainment</option>
-                <option>Health</option>
-                <option>Science</option>
-                <option>Sports</option>
-                <option>Technology</option>
-            </CategoryDropdown>
-            <CountryDropdown>{renderedCountryOptions}</CountryDropdown>
+            <SectionsContainer>
+                <CategoryList>
+                    <li>Business</li>
+                    <li>Entertainment</li>
+                    <li>Health</li>
+                    <li>Science</li>
+                    <li>Sports</li>
+                    <li>Technology</li>
+                </CategoryList>
+                <CategoryDropdown defaultValue="Categories">
+                    <option disabled hidden>
+                        Categories
+                    </option>
+                    <option>General</option>
+                    <option>Business</option>
+                    <option>Entertainment</option>
+                    <option>Health</option>
+                    <option>Science</option>
+                    <option>Sports</option>
+                    <option>Technology</option>
+                </CategoryDropdown>
+                <CountryDropdown defaultValue="Countries">
+                    <option disabled hidden>
+                        Countries
+                    </option>
+                    {renderedCountryOptions}
+                </CountryDropdown>
+                <SectionDropdown defaultValue="Sections">
+                    <optgroup label="Categories">
+                        <option disabled hidden>
+                            Sections
+                        </option>
+                        <option>General</option>
+                        <option>Business</option>
+                        <option>Entertainment</option>
+                        <option>Health</option>
+                        <option>Science</option>
+                        <option>Sports</option>
+                        <option>Technology</option>
+                    </optgroup>
+                    <optgroup label="Countries">{renderedCountryOptions}</optgroup>
+                </SectionDropdown>
+            </SectionsContainer>
         </Container>
     );
 };
--- src/components/Navigation/styles.js
+++ src/components/Navigation/styles.js
@@ -6,12 +6,23 @@ export const Container = styled.nav`
     display: grid;
     grid-gap: 0.5rem;
     width: 100%;
-    grid-template-columns: auto 1fr auto;
+    grid-template-columns: auto 1fr;
 `;
 
 export const Logo = styled.div`
     font-size: 1.5rem;
     font-weight: bold;
+    cursor: pointer;
+`;
+
+export const SectionsContainer = styled.div`
+    display: grid;
+    grid-gap: 0.5rem;
+    grid-template-columns: 1fr auto;
+    @media (${QUERIES.small}) {
+        justify-items: center;
+        justify-content: center;
+    }
 `;
 
 export const CategoryList = styled.ul`
@@ -22,16 +33,18 @@ export const CategoryList = styled.ul`
     li {
         padding: 0 1rem;
         font-size: 1rem;
+        cursor: pointer;
+        &:hover {
+            text-decoration: underline;
+        }
     }
     @media (${QUERIES.medium}) {
         display: none;
     }
 `;
 
-export const CategoryDropdown = styled.select`
-    display: none;
+const Dropdown = styled.select`
     overflow: hidden;
-    justify-self: end;
     border: 2px solid white;
     font-weight: bold;
     background: url(${arrowSVG}) no-repeat 95%;
@@ -39,32 +52,41 @@ export const CategoryDropdown = styled.select`
     -webkit-appearance: none;
     color: white;
     font-size: 1rem;
+    padding-left: 4px;
+    padding-right: 14px;
     cursor: pointer;
+    optgroup,
     option {
-        border: solid 2px white;
         font-weight: normal;
+        color: black;
         padding: 1rem;
-    }
-    @media (${QUERIES.medium}) {
-        display: flex;
     }
 `;
 
-export const CountryDropdown = styled.select`
-    /* display: none; */
-    overflow: hidden;
+export const CategoryDropdown = styled(Dropdown)`
+    display: none;
     justify-self: end;
-    border: 2px solid white;
-    font-weight: bold;
-    background: url(${arrowSVG}) no-repeat 95%;
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    color: white;
-    font-size: 1rem;
-    cursor: pointer;
-    option {
-        border: solid 2px white;
-        font-weight: normal;
-        padding: 1rem;
+    @media (${QUERIES.medium}) {
+        display: block;
+    }
+    @media (${QUERIES.small}) {
+        display: none;
+    }
+`;
+
+export const CountryDropdown = styled(Dropdown)`
+    justify-self: end;
+    max-width: 8rem;
+    @media (${QUERIES.small}) {
+        display: none;
+    }
+`;
+
+export const SectionDropdown = styled(Dropdown)`
+    display: none;
+    justify-self: end;
+    max-width: 8rem;
+    @media (${QUERIES.small}) {
+        display: block;
     }
 `;

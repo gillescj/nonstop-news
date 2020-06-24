6806d49cae0825168a163c27923203014e6df268
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-24 02:02:27 -0400

    Remove default dummy variables

--- src/components/NewsCards/TextNewsCard/TextNewsCardItem/index.js
+++ src/components/NewsCards/TextNewsCard/TextNewsCardItem/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Container, TextContent, ArticleTitle, ArticleInfo } from './styles';
-import faker from 'faker';
 import moment from 'moment';
 
-const TextNewsCardItem = ({
-    article = {
-        title: faker.lorem.sentences(Math.floor(Math.random() * 3) + 1),
-        url:
-            'https://www.fxstreet.com/news/forex-today-double-stimulus-talk-downs-dollar-ahead-of-powells-power-play-us-retail-sales-202006160607',
-    },
-}) => {
+const TextNewsCardItem = ({ article }) => {
     return (
-        <Container href={article.url} rel="noopener noreferrer" target="_blank">
-            <TextContent>
-                <ArticleTitle>{article.title}</ArticleTitle>
-                <ArticleInfo>{moment.utc(article.publishedAt).fromNow()}</ArticleInfo>
-            </TextContent>
-        </Container>
+        <>
+            {!article ? null : (
+                <Container href={article.url} rel="noopener noreferrer" target="_blank">
+                    <TextContent>
+                        <ArticleTitle>{article.title}</ArticleTitle>
+                        <ArticleInfo>
+                            {moment.utc(article.publishedAt).fromNow()}
+                        </ArticleInfo>
+                    </TextContent>
+                </Container>
+            )}
+        </>
     );
 };
 

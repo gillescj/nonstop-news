f9e28682463a3836d2ca232941486e58325c574b
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-23 21:15:54 -0400

    Add to Footer - Country choice, API attribution

--- src/components/MainFooter/index.js
+++ src/components/MainFooter/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Attribution, CountryContainer } from './styles';
+import acceptedCountries from 'utils/acceptedCountries';
 
 const MainFooter = () => {
-    return <Container>MainFooter</Container>;
+    const renderedCountryLinks = acceptedCountries.map((country) => {
+        return <h4 key={country.code}>{country.name}</h4>;
+    });
+
+    return (
+        <Container>
+            <Attribution>
+                Made possible with{' '}
+                <a href="https://newsapi.org/" target="_blank" rel="noopener noreferrer">
+                    NewsAPI
+                </a>
+            </Attribution>
+            <CountryContainer>{renderedCountryLinks}</CountryContainer>
+        </Container>
+    );
 };
 
 export default MainFooter;
--- src/components/MainFooter/styles.js
+++ src/components/MainFooter/styles.js
@@ -1,10 +1,35 @@
 import styled from 'styled-components';
-import { COLOURS } from 'styling';
+import { QUERIES, COLOURS } from 'styling';
 
-export const Container = styled.div`
+export const Container = styled.footer`
     display: grid;
     grid-gap: 1rem;
     padding: 1rem;
+    justify-items: space-between;
+    width: 100%;
     color: white;
     background: ${COLOURS.darkPurple};
+`;
+
+export const Attribution = styled.div`
+    border-bottom: 1px solid white;
+    padding-bottom: 1rem;
+    text-align: center;
+    a {
+        font-weight: bold;
+    }
+`;
+
+export const CountryContainer = styled.div`
+    display: grid;
+    padding: 1rem;
+    grid-row-gap: 1rem;
+    grid-auto-flow: column;
+    grid-template-rows: repeat(8, minmax(0.5rem, 1rem));
+    @media (${QUERIES.medium}) {
+        grid-template-rows: repeat(10, 1rem);
+    }
+    @media (${QUERIES.smallMedium}) {
+        display: none;
+    }
 `;

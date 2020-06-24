e81d6ef66a183b3de8e91691918c295374d740a7
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-23 23:01:31 -0400

    Remove default dummy values

--- src/components/NewsCards/PrimaryNewsCard/index.js
+++ src/components/NewsCards/PrimaryNewsCard/index.js
@@ -6,23 +6,9 @@ import {
     ArticleMainText,
     ArticleInfo,
 } from './styles';
-import faker from 'faker';
 import moment from 'moment';
 
-const PrimaryNewsCard = ({
-    article = {
-        source: {
-            name: 'FXStreet',
-        },
-        author: 'Yohay Elam',
-        title: faker.lorem.sentences(Math.floor(Math.random() * 3) + 1),
-        description: faker.lorem.paragraphs(Math.floor(Math.random() * 2) + 1),
-        publishedAt: '2020-06-16T06:07:43Z',
-        url:
-            'https://www.fxstreet.com/news/forex-today-double-stimulus-talk-downs-dollar-ahead-of-powells-power-play-us-retail-sales-202006160607',
-        urlToImage: faker.image.image(),
-    },
-}) => {
+const PrimaryNewsCard = ({ article }) => {
     const limitTextSize = (text, limit) => {
         if (!text) return;
         if (text.length > limit) return;
@@ -30,22 +16,26 @@ const PrimaryNewsCard = ({
     };
 
     return (
-        <Container href={article.url} rel="noopener noreferrer" target="_blank">
-            <ImageWrapper>
-                <img src={article.urlToImage} alt={article.title} />
-            </ImageWrapper>
-            <TextContent>
-                <ArticleMainText>
-                    <h3>{article.title}</h3>
-                    <p>{article.description}</p>
-                </ArticleMainText>
-                <ArticleInfo>
-                    <div>{limitTextSize(article.author, 20)}</div>
-                    <div>{limitTextSize(article.source.name, 20)}</div>
-                    <div>{moment.utc(article.publishedAt).fromNow()}</div>
-                </ArticleInfo>
-            </TextContent>
-        </Container>
+        <>
+            {!article ? null : (
+                <Container href={article.url} rel="noopener noreferrer" target="_blank">
+                    <ImageWrapper>
+                        <img src={article.urlToImage} alt={article.title} />
+                    </ImageWrapper>
+                    <TextContent>
+                        <ArticleMainText>
+                            <h3>{article.title}</h3>
+                            <p>{article.description}</p>
+                        </ArticleMainText>
+                        <ArticleInfo>
+                            <div>{limitTextSize(article.author, 20)}</div>
+                            <div>{limitTextSize(article.source.name, 20)}</div>
+                            <div>{moment.utc(article.publishedAt).fromNow()}</div>
+                        </ArticleInfo>
+                    </TextContent>
+                </Container>
+            )}
+        </>
     );
 };
 

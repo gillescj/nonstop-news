da4e3affd1e024f70c8c1748fd71b1a13f3bee56
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-25 19:47:05 -0400

    Add settings slice to redux store

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import articlesReducer from './articles';
+import settingsReducer from './settings';
 
 export default combineReducers({
     articles: articlesReducer,
+    settings: settingsReducer,
 });
---
+++ src/store/settings.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const settingsSlice = createSlice({
+    name: 'settings',
+    initialState: {
+        category: 'General',
+        country: { code: 'ca', name: 'Canada' },
+    },
+    reducers: {
+        changeCategory: (state, action) => {
+            state.category = action.payload;
+        },
+        changeCountry: (state, action) => {
+            state.country = action.payload;
+        },
+    },
+});
+
+export const { changeCategory, changeCountry } = settingsSlice.actions;
+export default settingsSlice.reducer;

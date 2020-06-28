9c53ee92d2a1fa919ceeb291cf41460d63ce933e
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-28 15:04:30 -0400

    Hook up dropdowns to redux store Change imports for acceptedCountries Change categoryNameList into categories with list and object data Alter option onClick events to select onChange events

--- src/components/MainHeader/Navigation/index.js
+++ src/components/MainHeader/Navigation/index.js
@@ -10,26 +10,58 @@ import {
     CountryDropdown,
     SectionDropdown,
 } from './styles';
-import acceptedCountries from 'utils/acceptedCountries';
+import { acceptedCountriesList, acceptedCountriesObject } from 'utils/acceptedCountries';
 
-const sortedCountries = acceptedCountries.sort((a, b) => {
+const sortedCountries = acceptedCountriesList.sort((a, b) => {
     return a.name > b.name;
 });
 
 const Navigation = () => {
     const dispatch = useDispatch();
 
-    const categoryNameList = [
-        { code: 'regional', name: 'National' },
-        { code: 'business', name: 'Business' },
-        { code: 'entertainment', name: 'Entertainment' },
-        { code: 'health', name: 'Health' },
-        { code: 'science', name: 'Science' },
-        { code: 'sports', name: 'Sports' },
-        { code: 'technology', name: 'Technology' },
-    ];
+    const categories = {
+        list: [
+            { code: 'regional', name: 'National' },
+            { code: 'business', name: 'Business' },
+            { code: 'entertainment', name: 'Entertainment' },
+            { code: 'health', name: 'Health' },
+            { code: 'science', name: 'Science' },
+            { code: 'sports', name: 'Sports' },
+            { code: 'technology', name: 'Technology' },
+        ],
+        object: {
+            regional: { code: 'regional', name: 'National' },
+            business: { code: 'business', name: 'Business' },
+            entertainment: { code: 'entertainment', name: 'Entertainment' },
+            health: { code: 'health', name: 'Health' },
+            science: { code: 'science', name: 'Science' },
+            sports: { code: 'sports', name: 'Sports' },
+            technology: { code: 'technology', name: 'Technology' },
+        },
+    };
 
-    const renderedCategoryList = categoryNameList.map((category) => {
+    const handleCategoryDropdownChange = (event) => {
+        dispatch(changeCategory(categories.object[event.target.value]));
+        event.target.value = 'Categories';
+    };
+
+    const handleCountryDropdownChange = (event) => {
+        dispatch(changeCountry(acceptedCountriesObject[event.target.value]));
+        event.target.value = 'Countries';
+    };
+
+    const handleSectionDropdownChange = (event) => {
+        const sectionType =
+            event.target.options[event.target.selectedIndex].parentNode.label;
+        if (sectionType === 'Categories') {
+            dispatch(changeCategory(categories.object[event.target.value]));
+        } else if (sectionType === 'Countries') {
+            dispatch(changeCountry(acceptedCountriesObject[event.target.value]));
+        }
+        event.target.value = 'Sections';
+    };
+
+    const renderedCategoryList = categories.list.map((category) => {
         return (
             <li
                 key={category.code}
@@ -42,14 +74,9 @@ const Navigation = () => {
         );
     });
 
-    const renderedCategoryOptions = categoryNameList.map((category) => {
+    const renderedCategoryOptions = categories.list.map((category) => {
         return (
-            <option
-                key={category.code}
-                onClick={() =>
-                    dispatch(changeCategory({ code: category.code, name: category.name }))
-                }
-            >
+            <option key={category.code} value={category.code}>
                 {category.name}
             </option>
         );
@@ -57,12 +84,7 @@ const Navigation = () => {
 
     const renderedCountryOptions = sortedCountries.map((country) => {
         return (
-            <option
-                key={country.code}
-                onClick={() =>
-                    dispatch(changeCountry({ code: country.code, name: country.name }))
-                }
-            >
+            <option key={country.code} value={country.code}>
                 {country.name}
             </option>
         );
@@ -81,7 +103,7 @@ const Navigation = () => {
                 <CategoryList>{renderedCategoryList}</CategoryList>
                 <CategoryDropdown
                     defaultValue="Categories"
-                    onChange={(event) => (event.target.value = 'Categories')}
+                    onChange={(event) => handleCategoryDropdownChange(event)}
                 >
                     <option disabled hidden>
                         Categories
@@ -90,7 +112,7 @@ const Navigation = () => {
                 </CategoryDropdown>
                 <CountryDropdown
                     defaultValue="Countries"
-                    onChange={(event) => (event.target.value = 'Countries')}
+                    onChange={(event) => handleCountryDropdownChange(event)}
                 >
                     <option disabled hidden>
                         Countries
@@ -99,7 +121,7 @@ const Navigation = () => {
                 </CountryDropdown>
                 <SectionDropdown
                     defaultValue="Sections"
-                    onChange={(event) => (event.target.value = 'Sections')}
+                    onChange={(event) => handleSectionDropdownChange(event)}
                 >
                     <optgroup label="Categories">
                         <option disabled hidden>

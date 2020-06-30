c18f38b373479fa84506e03d083b283fedf1b44b
Colin Gillespie <ratchetandcolin@gmail.com>
2020-06-29 21:42:22 -0400

    Add main routes and redirect

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from './style';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import MainContainer from '../MainContainer';
 import MainHeader from '../MainHeader';
 import MainFooter from '../MainFooter';
@@ -8,7 +9,12 @@ const App = () => {
     return (
         <Container>
             <MainHeader />
-            <MainContainer />
+            <Switch>
+                <Route path={'/:country'} exact component={MainContainer} />
+                <Route path={'/:country/:category'} exact component={MainContainer} />
+                <Redirect to={'/ca'} />
+            </Switch>
+
             <MainFooter />
         </Container>
     );
